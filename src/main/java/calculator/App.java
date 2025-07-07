18b0026544feb84f905f224006fb1523ceccf39c
liebigbear <jtw10155@naver.com>
2025-07-07 21:33:12 +0900

    fix: 연산 결과 저장 시 인덱스 증가 및 예외 처리 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -25,6 +25,8 @@ public class App {
             /* CharAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index) */
             char operator = sc.next().charAt(0);
 
+            boolean valid = true;
+
             int result = 0;
             if(operator == '+') {
                 result = a + b;
@@ -33,15 +35,23 @@ public class App {
             } else if(operator == '*') {
                 result = a * b;
             } else if(operator == '/') {
-                if(b == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                if(b == 0)  {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    valid = false;
+                }
                 else result = a / b;
             } else if(operator == '%') {
                 result = a % b;
             } else {
                 System.out.println("잘못된 연산자를 입력하셨습니다.");
+                valid = false;
             }
 
-            memoResult[memoResultIdx] = result;
+            if(valid) {
+                if(memoResult[memoResultIdx] < 10) {
+                    memoResult[memoResultIdx++] = result;
+                }
+            }
             System.out.println("결과: " + result);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
