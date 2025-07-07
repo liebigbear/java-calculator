24352b9de52e3d74154b0c0fa0d33341e03b1a57
liebigbear <jtw10155@naver.com>
2025-07-07 20:01:24 +0900

    feat: Scanner로 정수 2개를 입력받는 기능 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,21 @@
 package calculator;
 
+import java.util.Scanner;
+
+
 public class App {
-
+    /**
+     * 두 정수를 입력받아 출력하는 프로그램
+     * @param args
+     */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int a = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int b = sc.nextInt();
+
+        sc.close();
     }
 }
