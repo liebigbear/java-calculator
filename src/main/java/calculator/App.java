7cdb10bc2ce058fa1ccf9f23016f3739dabed3f1
liebigbear <jtw10155@naver.com>
2025-07-07 20:16:51 +0900

    feat: 입력받은 정수 2개와 사칙연산 기호를 사용하여 연산을 진행하는 기능 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -20,6 +20,25 @@ public class App {
         /* CharAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index) */
         char operator = sc.next().charAt(0);
 
+        int result = 0;
+        if(operator == '+') {
+            result = a + b;
+        } else if(operator == '-') {
+            result = a - b;
+        } else if(operator == '*') {
+            result = a * b;
+        } else if(operator == '/') {
+            if(b == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            else result = a / b;
+        } else if(operator == '%') {
+            result = a % b;
+        } else {
+            System.out.println("잘못된 연산자를 입력하셨습니다.");
+        }
+
+
+        System.out.println("결과: " + result);
+
         sc.close();
     }
 }
