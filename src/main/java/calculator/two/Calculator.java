ee5a15433ca8b8df37700dcb1b00b10a6ad92cfe
liebigbear <jtw10155@naver.com>
2025-07-08 16:13:39 +0900

    feat: 에러 메세지도 결과에 저장하는 기능 추가

--- src/main/java/calculator/two/Calculator.java
+++ src/main/java/calculator/two/Calculator.java
@@ -5,29 +5,33 @@ import java.util.Queue;
 
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
-    private Queue<Integer> memoResult = new LinkedList<>();
+    private Queue<String> memoResult = new LinkedList<>();
 
 
-    public Queue<Integer> calculate(int a, int b, char operator) {
-        int result = 0;
-        if(operator == '+') {
-            result = a + b;
-        } else if(operator == '-') {
-            result = a - b;
-        } else if(operator == '*') {
-            result = a * b;
-        } else if(operator == '/') {
-            if(b == 0)  {
-                //매개변수로 해당 오류 내용 전달
-                throw new ArithmeticException("0으로 나눌 수 없습니다.");
+    public Queue<String> calculate(int a, int b, char operator) {
+        try {
+            int result = 0;
+            if(operator == '+') {
+                result = a + b;
+            } else if(operator == '-') {
+                result = a - b;
+            } else if(operator == '*') {
+                result = a * b;
+            } else if(operator == '/') {
+                if(b == 0)  {
+                    //매개변수로 해당 오류 내용 전달
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                }
+                else result = a / b;
+            } else if(operator == '%') {
+                result = a % b;
+            } else {
+                throw new IllegalArgumentException("잘못된 연산자를 입력하셨습니다.");
             }
-            else result = a / b;
-        } else if(operator == '%') {
-            result = a % b;
-        } else {
-            throw new IllegalArgumentException("잘못된 연산자를 입력하셨습니다.");
+            memoResult.add(String.valueOf(result));
+        } catch (Exception e) {
+            memoResult.add(e.getMessage());
         }
-        memoResult.add(result);
         return memoResult;
     }
 }
