56404d8d3d9436884f4c8e7f13ec20e8d25b368c
liebigbear <jtw10155@naver.com>
2025-07-08 15:31:50 +0900

    refactor: 폴더 구조 변경

--- src/main/java/calculator/one/App.java
+++ src/main/java/calculator/one/App.java
@@ -1,6 +1,5 @@
-package calculator;
+package calculator.one;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
--- src/main/java/calculator/one/Calculator.java
+++ src/main/java/calculator/one/Calculator.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator.one;
 
 public class Calculator {
 
